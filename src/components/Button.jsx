62b893e78aa98114b1477acdf74d08167b02bc78
nowyourejinxed <sabrinajackson@nevada.unr.edu>
2025-03-04 23:01:14 -0800

    [fix] fix import typo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import ButtonSvg from "../assets/ButtonSvg";
+import ButtonSvg from "../assets/ButtonSVG";
 
 export default function Button ({ className, href, onClick, children, px, white }){
     const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
