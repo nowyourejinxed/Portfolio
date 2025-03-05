3dc327016b7ad157ba0fb9a00397ccf2f1068ce6
nowyourejinxed <sabrinajackson@nevada.unr.edu>
2025-03-04 23:37:57 -0800

    vanquish vercel?

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  server: {
-    //port: process.env.PORT as unknown as number,
-  },
   plugins: [react(), vercel()],
 })
