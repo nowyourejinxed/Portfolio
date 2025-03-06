9767dd3b1e8bdf10b391b7e64c1423acfc87cc15
nowyourejinxed <sabrinajackson@nevada.unr.edu>
2025-03-05 19:52:09 -0800

    update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,7 @@ export default {
         sans: ["var(--font-sora)", ...fontFamily.sans],
         code: "var(--font-code)",
         grotesk: "var(--font-grotesk)",
+        PressStart2P: "var(--font-press)"
       },
       letterSpacing: {
         tagline: ".15em",
